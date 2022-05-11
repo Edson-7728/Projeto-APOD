fea41f1054dff4a5a95c8eb625951f82df9d19b3
Edson Vieira <elvieira.275@gmail.com>
2022-05-11 15:35:05 -0300

    Subindo projeto Apod para repositório com commit 1

---
+++ apod.js
@@ -0,0 +1,27 @@
+let botao = $ ('#btnDeBusca').click(function () {
+         
+    let data = $ ('#ddMMAAAA').val ();
+   
+  
+    $.ajax ({
+        url: `https://api.nasa.gov/planetary/apod?api_key=EITyOD4rcZcFcz6Vn6KBc6iHdJ176LVrOuhMojeH&date=${data}`,
+        type: "GET",
+        success: function(retorna){
+            if (retorna.media_type=='video'){
+                $(`#filme`).html(`<iframe id='videoLink' src="${retorna.url}"></iframe>`);
+                $(`.divfoto`).css('display', 'none');
+            } else {
+                $(`#imagem`).html(`<img src="${retorna.url}">`);
+                $(`.divvideo`).css('display', 'none');
+            }
+            console.log(retorna);                    
+        $('#mensDescricao').text(retorna.explanation);
+        $('#imagem').attr("src", retorna.url);
+        $('#filme').attr("src", retorna.url);
+        $('#mensTitulo').text(retorna.title);                
+        },
+        error: function(error) {
+            console.log(error);
+        }
+    })
+})
